1d9b07a920103e958d4b716df1c9fc077b21e087
vonfrancis00 <vonacenas17@gmail.com>
2026-04-28 13:05:14 +0800

    for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "/api"
 });
--- server/server.js
+++ server/server.js
@@ -5,18 +5,34 @@ require("dotenv").config();
 
 const app = express();
 
+/* Middleware */
 app.use(cors());
 app.use(express.json());
 
+/* Routes */
 app.use("/api/transmittals", require("./routes/transmittalRoutes"));
 
-mongoose.connect(process.env.MONGO_URI)
+/* Health Check */
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+/* MongoDB */
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
     console.log("Connected DB:", mongoose.connection.name);
 
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
-    });
+    /* Localhost Only */
+    if (process.env.NODE_ENV !== "production") {
+      const PORT = process.env.PORT || 5000;
+
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    }
   })
   .catch((err) => console.log(err));
+
+module.exports = app;
